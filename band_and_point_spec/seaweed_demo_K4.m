7e71f8ae9c9d201f634dde4a7e0c8c7505a64f1e
Fabian Gabel <fabian.gabel@tuhh.de>
2021-07-12 21:44:04 +0200

    adapt demo scripts

--- band_and_point_spec/seaweed_demo_K4.m
+++ band_and_point_spec/seaweed_demo_K4.m
@@ -1,9 +1,25 @@
 % K4
-export_spectrum('data/K4_lambda.csv', 'data/K4_v.csv',nPoints=10^5);
-createTexPointSpec('points.csv')
-createTexBandSpec('bands.csv')
-movefile('bands.csv','data/K4_bands.csv')
-movefile('points.csv','data/K4_points.csv')
-movefile('bands.tex','data/K4_bands.tex')
-movefile('points.tex','data/K4_points.tex')
+
+% set parameters
+file_prefix='data/K4_';
+lambda_file = [file_prefix, 'lambda.csv'];
+v_file      = [file_prefix, 'v.csv'];
+nPoints=10^5;
+bandspec=true;
+pointspec=true;
+limops=true;
+write2file=true;
+visualize=false;
+
+%read in data
+lambda = dlmread(lambda_file);
+v = dlmread(v_file);
+
+% calculate spectrum
+reproduce_seaweed(lambda, v,  nPoints, bandspec, pointspec,...
+                   limops, write2file, file_prefix, visualize)
+
+% create tex
+createTexPointSpec([file_prefix,'points.csv'])
+createTexBandSpec([file_prefix,'bands.csv'])
 
--- band_and_point_spec/seaweed_demo_K9.m
+++ band_and_point_spec/seaweed_demo_K9.m
@@ -1,8 +1,30 @@
 %K9
 
-reproduce_seaweed(0.01, [1,1,0,1,0,1,0,1,1],  3*10^5);
-export_spectrum('data/K9_lambda.csv', 'data/K9_v.csv',nPoints=10^5);
-createTexPointSpec('points.csv')
-createTexBandSpec('bands.csv')
-movefile('bands.csv','data/K9_bands.csv')
-movefile('points.csv','data/K9_points.csv')
+% set parameters
+file_prefix='data/K9_';
+lambda_file = [file_prefix, 'lambda.csv'];
+v_file      = [file_prefix, 'v.csv'];
+nPoints=3*10^5;
+bandspec=true;
+pointspec=true;
+limops=true;
+write2file=true;
+visualize=false;
+
+%read in data
+lambda = 0.01;
+v = dlmread(v_file);
+
+% calculate spectrum 2 steps
+reproduce_seaweed(lambda, v,  nPoints, bandspec, pointspec,...
+                   limops, write2file, file_prefix, visualize)
+
+nPoints=10^5;
+lambda = dlmread(lambda_file);
+reproduce_seaweed(lambda, v,  nPoints, bandspec, pointspec,...
+                   limops, write2file, file_prefix, visualize)
+
+% create tex
+createTexPointSpec([file_prefix,'points.csv'])
+createTexBandSpec([file_prefix,'bands.csv'])
+
