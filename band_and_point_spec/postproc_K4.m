24a0925d2784e943fb6a221ccd979636032872fa
Fabian Gabel <fabian.gabel@tuhh.de>
2021-08-12 17:38:59 +0200

    postprocessing for K4

--- band_and_point_spec/postproc_K4.m
+++ band_and_point_spec/postproc_K4.m
@@ -4,7 +4,9 @@
 file_prefix='data/';
 
 % create tex
-createTexTrajectory([file_prefix,'0111_0.csv'])
 createTexTrajectory([file_prefix,'1101_1.csv'])
+createTexTrajectory([file_prefix,'1101_2.csv'])
+createTexTrajectory([file_prefix,'0111_0.csv'])
+
 createTexBandSpec([file_prefix,'K4_bands.csv'])
 
