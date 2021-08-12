c9aa8b8a95f3bb07d8d2f39ed09a640140bc05b5
Fabian Gabel <fabian.gabel@tuhh.de>
2021-08-12 12:51:27 +0200

    add postprocessing routines

---
+++ band_and_point_spec/postproc_K4.m
@@ -0,0 +1,10 @@
+% K4
+
+% set parameters
+file_prefix='data/';
+
+% create tex
+createTexTrajectory([file_prefix,'0111_0.csv'])
+createTexTrajectory([file_prefix,'1101_1.csv'])
+createTexBandSpec([file_prefix,'K4_bands.csv'])
+
---
+++ band_and_point_spec/postproc_K5.m
@@ -0,0 +1,12 @@
+% K5
+
+% set parameters
+file_prefix='data/';
+
+% create tex
+createTexTrajectory([file_prefix,'01011_0.csv'])
+createTexTrajectory([file_prefix,'01011_1.csv'])
+createTexTrajectory([file_prefix,'10110_0.csv'])
+createTexTrajectory([file_prefix,'11010_1.csv'])
+createTexBandSpec([file_prefix,'K5_bands.csv'])
+
