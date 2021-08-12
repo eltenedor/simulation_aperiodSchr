39bc0ef70022531905cda65de650325113cfd015
Fabian Gabel <fabian.gabel@tuhh.de>
2021-08-12 17:38:48 +0200

    add K5 trajectory

--- band_and_point_spec/postproc_K5.m
+++ band_and_point_spec/postproc_K5.m
@@ -4,9 +4,13 @@
 file_prefix='data/';
 
 % create tex
+createTexTrajectory([file_prefix,'11010_1.csv'])
+createTexTrajectory([file_prefix,'11010_3.csv'])
+createTexTrajectory([file_prefix,'10110_0.csv'])
+createTexTrajectory([file_prefix,'10110_2.csv'])
 createTexTrajectory([file_prefix,'01011_0.csv'])
 createTexTrajectory([file_prefix,'01011_1.csv'])
-createTexTrajectory([file_prefix,'10110_0.csv'])
-createTexTrajectory([file_prefix,'11010_1.csv'])
+createTexTrajectory([file_prefix,'10101_2.csv'])
+createTexTrajectory([file_prefix,'10101_3.csv'])
 createTexBandSpec([file_prefix,'K5_bands.csv'])
 
