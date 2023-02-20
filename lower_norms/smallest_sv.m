4677c59fc829e76069628a92fbed099368d51132
Riko Ukena <cru8402@pc11.mat.tu-harburg.de>
2023-02-20 14:09:34 +0100

    fixes for octave

--- lower_norms/smallest_sv.m
+++ lower_norms/smallest_sv.m
@@ -2,5 +2,5 @@ function [min_sv] = smallest_sv(v)
     n = length(v);
     H = diag(v) + diag(ones(n-1,1), 1) + diag(ones(n-1,1), -1);
     H = [H; [zeros(1, n-1) , 1]];
-    min_sv = svds(H, 1, 'smallest');
+    min_sv = min(svd(H));
 end
